f81dd57ecc2a0e990b4bd968711bbcd01f269a1c
Henrique Akiyoshi Eguchi <henrique.eguchi@outlook.com>
2024-01-04 16:51:50 -0300

    Input in Java

--- dsa/java-bootcamp-kunal/first-idea-program/src/com/henrique/Main.java
+++ dsa/java-bootcamp-kunal/first-idea-program/src/com/henrique/Main.java
@@ -1,6 +1,10 @@
 package com.henrique;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
