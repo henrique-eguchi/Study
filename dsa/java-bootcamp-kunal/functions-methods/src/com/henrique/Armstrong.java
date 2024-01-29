11ac0d99167ebc8413d98668a2d1d72c10584a21
Henrique Akiyoshi Eguchi <henrique.eguchi@outlook.com>
2024-01-28 22:02:44 -0300

    Add Armstrong and Prime programs

---
+++ dsa/java-bootcamp-kunal/functions-methods/src/com/henrique/Armstrong.java
@@ -0,0 +1,26 @@
+package com.henrique;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        for(int i = 100; i <= 1000; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum += rem*rem*rem;
+        }
+
+        return sum == original;
+    }
+}
